91e43839f01c04530d0885dcb9f74cab9375c03d
kimdoan <doan0105@e3view.com>
2021-12-20 21:33:05 +0900

    First Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,15 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Notion-Mini",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+    "gatsby-plugin-mdx",
+  ],
 };
---
+++ src/components/layout.js
@@ -0,0 +1,59 @@
+import * as React from 'react'
+import { graphql, Link, useStaticQuery } from 'gatsby'
+import {
+  container,
+  heading,
+  navLinks,
+  navLinkItem,
+  navLinkText,
+  siteTitle,
+  
+} from './layout.module.css'
+import SideBar from './sidebar'
+
+const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+    return (
+        <div>
+      <SideBar pageTitle={"test"}></SideBar>
+    <div className={container}>
+      <title>{pageTitle} | {data.site.siteMetadata.title}</title>
+      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
+      <nav>
+        <ul className={navLinks}>
+          <li className={navLinkItem}>
+            <Link to="/" className={navLinkText}>
+              Home
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/about" className={navLinkText}>
+              About
+            </Link>
+          </li>
+        <li className={navLinkItem}>
+            <Link to="/blog" className={navLinkText}>
+              Blog
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h1 className={heading}>{pageTitle}</h1>
+        {children}
+      </main>
+            </div>
+            </div>
+  )
+}
+
+export default Layout
