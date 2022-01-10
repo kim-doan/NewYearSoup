3fa2541ba77ea9e0964f0b74839d04d3ba09d9d1
kim-doan <doan960105@gmail.com>
2022-01-10 23:38:56 +0900

    no message

--- src/pages/table/[soupTable].jsx
+++ src/pages/table/[soupTable].jsx
@@ -5,13 +5,23 @@ import Layout from "../../components/layout";
 import { userAction, userSelector } from "../auth/slice";
 import NotFoundPage from "./notFound";
 import * as style from "./soupTable.module.css";
-
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const SoupTablePage = (props) => {
     const dispatch = useDispatch();
     const isSearchUser = useSelector(userSelector.isSearchUser);
     const isLoading = useSelector(userSelector.isLoading);
     const searchUserInfo = useSelector(userSelector.searchUserInfo);
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    };
 
     useEffect(() => {
         console.log(props.params.soupTable)
@@ -39,49 +49,91 @@ const SoupTablePage = (props) => {
                                 <div className={style.soupTableContainer}>
                                     <div className={style.titleContainer}>
                                         <label>{searchUserInfo.USER_NAME}님에게</label>
-                                        <label>떡국 1그릇이 배달됐어요!</label>
+                                        <label>떡국 1그릇이 배달됐어요!!</label>
                                     </div>
                                     <div className={style.soupTableBox}>
-                                        <div className={style.soupTable}>
-                                            <StaticImage
-                                                src="../../../assets/img/table.png"
-                                                alt="table"
-                                                layout="fixed"
-                                                width={350}
-                                            />
-                                            <div className={style.soupPostion01}>
+                                        <Slider {...settings}>
+                                            <div className={style.soupTable}>
                                                 <StaticImage
-                                                    src="../../../assets/icon/soup01.png"
-                                                    alt="soup01"
-                                                    layout="constrained"
-                                                    width={100}
+                                                    src="../../../assets/img/table.png"
+                                                    alt="table"
+                                                    layout="fixed"
+                                                    width={350}
                                                 />
+                                                <div className={style.soupPostion01}>
+                                                    <StaticImage
+                                                        src="../../../assets/icon/soup01.png"
+                                                        alt="soup01"
+                                                        layout="constrained"
+                                                        width={100}
+                                                    />
+                                                </div>
+                                                <div className={style.soupPostion02}>
+                                                    <StaticImage
+                                                        src="../../../assets/icon/soup02.png"
+                                                        alt="soup02"
+                                                        layout="constrained"
+                                                        width={100}
+                                                    />
+                                                </div>
+                                                <div className={style.soupPostion03}>
+                                                    <StaticImage
+                                                        src="../../../assets/icon/soup01.png"
+                                                        alt="soup03"
+                                                        layout="constrained"
+                                                        width={100}
+                                                    />
+                                                </div>
+                                                <div className={style.soupPostion04}>
+                                                    <StaticImage
+                                                        src="../../../assets/icon/soup02.png"
+                                                        alt="soup04"
+                                                        layout="constrained"
+                                                        width={100}
+                                                    />
+                                                </div>
                                             </div>
-                                            <div className={style.soupPostion02}>
+                                            <div className={style.soupTable}>
                                                 <StaticImage
-                                                    src="../../../assets/icon/soup02.png"
-                                                    alt="soup02"
-                                                    layout="constrained"
-                                                    width={100}
+                                                    src="../../../assets/img/table.png"
+                                                    alt="table"
+                                                    layout="fixed"
+                                                    width={350}
                                                 />
+                                                <div className={style.soupPostion01}>
+                                                    <StaticImage
+                                                        src="../../../assets/icon/soup01.png"
+                                                        alt="soup01"
+                                                        layout="constrained"
+                                                        width={100}
+                                                    />
+                                                </div>
+                                                <div className={style.soupPostion02}>
+                                                    <StaticImage
+                                                        src="../../../assets/icon/soup02.png"
+                                                        alt="soup02"
+                                                        layout="constrained"
+                                                        width={100}
+                                                    />
+                                                </div>
+                                                <div className={style.soupPostion03}>
+                                                    <StaticImage
+                                                        src="../../../assets/icon/soup01.png"
+                                                        alt="soup03"
+                                                        layout="constrained"
+                                                        width={100}
+                                                    />
+                                                </div>
+                                                <div className={style.soupPostion04}>
+                                                    <StaticImage
+                                                        src="../../../assets/icon/soup02.png"
+                                                        alt="soup04"
+                                                        layout="constrained"
+                                                        width={100}
+                                                    />
+                                                </div>
                                             </div>
-                                            <div className={style.soupPostion03}>
-                                                <StaticImage
-                                                    src="../../../assets/icon/soup01.png"
-                                                    alt="soup03"
-                                                    layout="constrained"
-                                                    width={100}
-                                                />
-                                            </div>
-                                            <div className={style.soupPostion04}>
-                                                <StaticImage
-                                                    src="../../../assets/icon/soup02.png"
-                                                    alt="soup04"
-                                                    layout="constrained"
-                                                    width={100}
-                                                />
-                                            </div>
-                                        </div>
+                                        </Slider>
                                     </div>
                                     {/* <div className={style.soupTableBox}>
                                             <StaticImage
